6603438862c4c989e9a2f09261503995bb3ea662
japaos <69161654+japaos@users.noreply.github.com>
2020-08-03 13:52:51 -0500

    Update funciones.js

--- html/funciones.js
+++ html/funciones.js
@@ -1,1 +1,15 @@
 //funciones en js
+
+function  crear_tabla(){
+
+    document.write("<table width=200 border=1 cellpadding=1 cellspacing=1>"); 
+for (i=0;i<temperaturas_cuidades.length;i++){ 
+   	document.write("<tr>") 
+   	document.write("<td><b>Ciudad " + i + "</b></td>") 
+   	for (j=0;j<temperaturas_cuidades[i].length;j++){ 
+      	document.write("<td>" + temperaturas_cuidades[i][j] + "</td>") 
+   	} 
+   	document.write("</tr>") 
+} 
+document.write("</table>")
+}
