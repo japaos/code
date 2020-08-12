c3366e75075f7eda54ee9b1d1c38324b6adb6a9a
japaos <69161654+japaos@users.noreply.github.com>
2020-08-12 13:34:54 -0500

    Update funcionesiniciales.js

--- html/res/funcionesiniciales.js
+++ html/res/funcionesiniciales.js
@@ -8,3 +8,34 @@ console.log(vector[n]);
 
 }
 
+
+function crear(nu){
+    switch(nu){
+        case 0:    
+            var miString = "Hola Amigos" 
+            var result = ""        
+            for (i=0;i<miString.length-1;i++) { 
+                result += miString.charAt(i) 
+                result += "-" 
+            } 
+            result += miString.charAt(miString.length - 1);
+
+            alert(result);
+            break;
+        case 1:
+            document.bgColor="#81F1F3";
+            break;
+        default:
+            var miString = "0123456789" 
+            var mitad1,mitad2
+            
+            posicion_mitad = miString.length / 2
+            
+            mitad1 = miString.substring(0,posicion_mitad) 
+            mitad2 = miString.substring(posicion_mitad,miString.length)
+            
+            document.write(mitad1 + "<br>" + mitad2)
+            break;            
+
+    }
+}
