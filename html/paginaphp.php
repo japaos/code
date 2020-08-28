0ae617e7885bc3b4c4c0128912ce3dc643b384a2
japaos <69161654+japaos@users.noreply.github.com>
2020-08-28 18:44:59 -0500

    Create paginaphp.php

---
+++ html/paginaphp.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8">
+    <title>Primera página PHP</title>
+</head>
+<body>
+    <h1>Esto es HTML</h1>
+    <?php
+    echo '<p>Esto viene de PHP</p>';
+    ?>
+</body>
+</html>
