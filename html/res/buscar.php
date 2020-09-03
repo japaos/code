2f430f5659621905ae302d9470f59a3e16d8cec0
japaos <69161654+japaos@users.noreply.github.com>
2020-09-03 14:14:53 -0500

    Create buscar.php

---
+++ html/res/buscar.php
@@ -0,0 +1,18 @@
+<HTML lang="es">
+<meta charset="UTF-8">    
+<HEad>
+Guarda
+<link rel="stylesheet" type="text/css" href="res/base.css">
+</HEad>
+<body>
+    <div id="contenedor">
+        <div id="t">
+            lakdfjldkjfkldjflkjdf
+        </div>
+        <div id="t1">
+            adafajas hadas dadas las lecciones ejecutadas por el usurio es imposible visualizar
+        </div>
+    </div>
+</body>
+
+</HTML>
